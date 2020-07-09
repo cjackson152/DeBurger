9349ee62823ce5cd7af6b62c3fa080b376160b56
cole jackson <cole_jackson@live.com>
2020-07-09 11:35:49 -0700

    database construction

---
+++ database/demo_seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO sandwiches (
+    burger_name,
+    eaten
+) VALUES
+(
+
+    "Cheese Burger",
+    0
+
+),
+(
+
+    "Truffle Burger",
+    0
+
+),
+(
+
+    "Pastrami Burger",
+    0
+
+);
