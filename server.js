d0db3837a09ce125aaba303437dc019795ef9c9e
cole jackson <cole_jackson@live.com>
2020-07-09 10:41:57 -0700

    building server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let methodOverride = require("method-override");
+
+let port = 3001;
+let app = express();
+app.use(express.static(process.cwd() + "/publc"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view enginge", "handlebars");
+let routes = require(".?controllers/burgers_controller.js");
+
+app.use("/", routes);
+app.listen(port);
+
+    
