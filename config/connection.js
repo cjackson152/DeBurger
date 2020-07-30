3cb75dcd389adedd5df2c12363cd4a3d63624690
Cole Jackson <cole_jackson@live.com>
2020-07-30 12:08:18 -0700

    forgot to commit

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 let mysql = require("mysql");
-let connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "mixVok-quxsy0-sinmup",
-    database: "burgers"
-});
+let connection;
+if( process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "mixVok-quxsy0-sinmup",
+        database: "burgers"
+    })
+}
+
 
 connection.connect(function(err) {
     if (err) {
